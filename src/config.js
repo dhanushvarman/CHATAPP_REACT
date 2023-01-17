bab78ec2371e378879c878b7f04517c991d0bb53
dhanushvarman <dhanushvarmanj@gmail.com>
2023-01-17 19:09:06 +0530

    config change

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const config = {
-    api : "http://localhost:3000",
-    mainPage : "http://localhost:3001"
+    api : "https://chatapp-main-server.vercel.app",
+    mainPage : "https://chatapp-react-weld.vercel.app"
 }
