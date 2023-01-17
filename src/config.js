53ded1764d752c555ca68eaa90c6972476210d6a
dhanushvarman <dhanushvarmanj@gmail.com>
2023-01-17 20:00:47 +0530

    last commit

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export const config = {
-    api : "https://chatapp-main-server.vercel.app",
+    api:"https://chatapp-server-ra0z.onrender.com",
+    // api : "https://chatapp-main-server.vercel.app",
     mainPage : "https://chatapp-react-weld.vercel.app"
 }
